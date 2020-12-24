968ea5689f58381e0d15db20e647fd651f336220
adelnizamutdinov <stiggpwnz@gmail.com>
2020-12-24 15:22:15 +0300

    nobind

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <string>
+#include "gvc.h"
 
 #include "emscripten.h"
-
-#include "gvc.h"
 
 extern gvplugin_library_t gvplugin_core_LTX_library;
 extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
@@ -12,14 +11,25 @@ extern int Nop;
 
 static std::string errorMessages;
 
-int vizErrorf(char *buf)
+int appendErr(char *buf)
 {
   errorMessages.append(buf);
   return 0;
 }
 
 EMSCRIPTEN_KEEPALIVE
-std::string version(const std::string src, const std::string format, const std::string engine, const int yInvert)
+std::string vizLastErrorMessage()
+{
+  if (agreseterrors() == 0)
+    return "";
+
+  std::string str(errorMessages);
+  errorMessages.clear();
+  return str;
+}
+
+EMSCRIPTEN_KEEPALIVE
+std::string layout(const std::string src, const std::string format, const std::string engine, const int yInvert)
 {
   Y_invert = yInvert;
 
@@ -28,7 +38,7 @@ std::string version(const std::string src, const std::string format, const std::
   gvAddLibrary(context, &gvplugin_dot_layout_LTX_library);
 
   agseterr(AGERR);
-  agseterrf(vizErrorf);
+  agseterrf(appendErr);
 
   const auto *input = src.c_str();
   char *output = NULL;
@@ -57,3 +67,9 @@ std::string version(const std::string src, const std::string format, const std::
 
   return result;
 }
+
+// EMSCRIPTEN_BINDINGS(graphviz)
+// {
+//   emscripten::function("layout", &layout);
+//   emscripten::function("error", &vizLastErrorMessage);
+// }
